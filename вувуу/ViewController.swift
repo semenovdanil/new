b8494a2621f83271a3ea46acf92f9000ed59060a
Гость <Guest@iMac-Mac005-3.local>
2022-04-07 14:00:12 +0900

    07.04

---
+++ вувуу/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  вувуу
+//
+//  Created by Гость on 06.04.2022.
+//
+/Users/Guest/Desktop/calculator-main/вувуу/
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
